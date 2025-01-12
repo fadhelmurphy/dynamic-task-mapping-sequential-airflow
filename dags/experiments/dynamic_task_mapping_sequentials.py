358778a96fc3d7242fe474a539933481dbba4352
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2025-01-12 07:57:40 +0700

    feat: update wording

--- dags/experiments/dynamic_task_mapping_sequentials.py
+++ dags/experiments/dynamic_task_mapping_sequentials.py
@@ -2,29 +2,26 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 
-MESSAGES_PER_TASK = 100_000  # Jumlah pesan per task
+CHUNK_SIZE_PER_TASK = 100_000  # Jumlah data per task
 
 def fetch_data():
-    messages = list(range(551_000))
+    data = list(range(551_000))
 
-    # Hitung jumlah task
-    total_messages = len(messages)
-    chunk_size = MESSAGES_PER_TASK
+    # jumlah task
+    total_data = len(data)
     chunks = [
-        {'chunk': messages[i:i + chunk_size]} for i in range(0, total_messages, chunk_size)
+        {'chunk': data[i:i + CHUNK_SIZE_PER_TASK]} for i in range(0, total_data, CHUNK_SIZE_PER_TASK)
     ]
 
-    print(f"Total messages: {total_messages}, Total tasks: {len(chunks)}")
+    print(f"Total data: {total_data}, Total tasks: {len(chunks)}")
     return chunks
 
 def process_chunk(chunk):
     """Proses chunk tertentu."""
-    print(f"Memproses {len(chunk)} pesan. Contoh pesan: {chunk[:5]}...")
+    print(f"Jumlah data: {len(chunk)} data. Contoh data: {chunk[:5]}...")
     for code in chunk:
-        # Simulasikan pemrosesan
         print(f"Processing code: {code}")
 
-# Definisi DAG
 with DAG(
     "dynamic_task_mapping_with_fixed_chunk_size",
     schedule_interval=None,
@@ -33,15 +30,13 @@ with DAG(
     concurrency=5,
 ) as dag:
 
-    # Task 1: Fetch data
     fetch_data_task = PythonOperator(
         task_id="fetch_data",
         python_callable=fetch_data,
     )
 
-    # Task 2: Process chunks dynamically
     process_chunk_task = PythonOperator.partial(
         task_id="process_chunk",
         python_callable=process_chunk,
-        max_active_tis_per_dag=1, # Sequentials
+        max_active_tis_per_dag=1, # Sequential
     ).expand(op_kwargs=fetch_data_task.output)
--- dags/experiments/dynamic_task_mapping_sequentials_decorator.py
+++ dags/experiments/dynamic_task_mapping_sequentials_decorator.py
@@ -3,37 +3,33 @@ from datetime import datetime
 from airflow.decorators import task
 from airflow.operators.python import PythonOperator
 
-MESSAGES_PER_TASK = 100_000  # Jumlah pesan per task
+CHUNK_SIZE_PER_TASK = 100_000  # Jumlah pesan per task
 
 @task
 def fetch_data():
-    messages = list(range(551_000))
+    data = list(range(551_000))
 
-    # Hitung jumlah task
-    total_messages = len(messages)
-    chunk_size = MESSAGES_PER_TASK
+    # jumlah task
+    total_data = len(data)
     chunks = [
-        messages[i:i + chunk_size] for i in range(0, total_messages, chunk_size)
+        data[i:i + CHUNK_SIZE_PER_TASK] for i in range(0, total_data, CHUNK_SIZE_PER_TASK)
     ]
 
-    print(f"Total messages: {total_messages}, Total tasks: {len(chunks)}")
+    print(f"Total data: {total_data}, Total tasks: {len(chunks)}")
 
     return chunks
 
 
 @task(
         task_id='process_data',
-        max_active_tis_per_dag=1, # Sequentials
+        max_active_tis_per_dag=1, # Sequential
         )
 def process_chunk(chunk):
     """Proses chunk tertentu."""
-    print(f"Memproses {len(chunk)} pesan. Contoh pesan: {chunk[:5]}...")
+    print(f"Jumlah data: {len(chunk)} data. Contoh data: {chunk[:5]}...")
     for code in chunk:
-        # Simulasikan pemrosesan
         print(f"Processing code: {code}")
 
-
-# Definisi DAG
 with DAG(
     "dynamic_task_mapping_with_fixed_chunk_size_decorator",
     schedule_interval=None,
@@ -42,8 +38,6 @@ with DAG(
     concurrency=5,
 ) as dag:
 
-    # Task 1: Fetch data
     chunks = fetch_data()
 
-    # Task 2: Generate tasks to process data chunks
     process_chunk.expand(chunk=chunks)
